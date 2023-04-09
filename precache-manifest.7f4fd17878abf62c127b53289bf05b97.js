d4d15fa9cb1f0effb2a2da290cce1f5c592d63d0
Shelby Shum <sshum00@gmail.com>
2023-04-09 14:11:53 -0700

    Updates

---
+++ precache-manifest.7f4fd17878abf62c127b53289bf05b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2087541a4872280f68b4",
+    "url": "/pour-over/static/css/main.d300498c.chunk.css"
+  },
+  {
+    "revision": "2087541a4872280f68b4",
+    "url": "/pour-over/static/js/main.2087541a.chunk.js"
+  },
+  {
+    "revision": "2e76ef4f4b07ab805959",
+    "url": "/pour-over/static/js/1.2e76ef4f.chunk.js"
+  },
+  {
+    "revision": "d8c50392f2b6cb6b0186",
+    "url": "/pour-over/static/js/2.d8c50392.chunk.js"
+  },
+  {
+    "revision": "5461007470ef8783e63b",
+    "url": "/pour-over/static/js/runtime~main.54610074.js"
+  },
+  {
+    "revision": "4d94b0dcb892fdc838412b6f632f72b2",
+    "url": "/pour-over/index.html"
+  }
+];
